368e275424197521959a3a35a002c522e1d6d8bd
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-03 12:34:57 +0100

    com1 - program.cs - główna klasa programu - punkt wejścia

--- ConsoleGames/Program.cs
+++ ConsoleGames/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConsoleGames
+﻿using System;
+using ConsoleGames.Helpers;
+
+// Piotr Bacior - Portfolio Project 2026
+
+namespace ConsoleGames
 {
+    // Główna klasa programu zawierająca punkt wejścia Main
     internal class Program
     {
+        // Punkt wejścia aplikacji
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Wywołuję metodę DisplayHeader z klasy AuthorInfo, aby wyświetlić nagłówek gry
+            AuthorInfo.DisplayHeader("Hub Gier Konsolowych");
+
+            // Wyświetlam powitanie użytkownika i instrukcje zamknięcia programu
+            Console.WriteLine("Witaj w projekcie gier konsolowych!");
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zamknąć...\n");
+            Console.ReadKey();
         }
     }
 }
