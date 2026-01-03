10d94a81d849b1e7ddbd1774d61cd15718ef6efb
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-03 12:32:14 +0100

    com1 - AuthorInfo - stworzenie metody DisplayHeader która wyświetla w konsoli odpowiednie informacje dla użytkownika

--- ConsoleGames/Helpers/AuthorInfo.cs
+++ ConsoleGames/Helpers/AuthorInfo.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Piotr Bacior - Portfolio Project 2026
+
 namespace ConsoleGames.Helpers
 {
-    internal class AuthorInfo
+    // Tworzę statyczną klasę AuthorInfo, która zawiera metodę DisplayHeader do wyświetlania informacji o autorze i tytule gry.
+    public static class AuthorInfo
     {
+        // Definiuję metodę DisplayHeader, która przyjmuje tytuł gry jako parametr i wyświetla go wraz z informacjami o autorze.
+        public static void DisplayHeader(string gameTitle)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine($"   PROJEKT PORTFOLIO: PIOTR BACIOR");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"    GRA: {gameTitle}\n");
+            Console.ResetColor();
+        }
     }
 }
