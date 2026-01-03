2bcdb9a12c4259441b2d8287a690e31fe32c21d9
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-03 12:41:22 +0100

    com1 - NumberGuessingGame.cs - inicjalizacja klasy

---
+++ ConsoleGames/Games/NumberGuessingGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleGames.Helpers;
+
+// Piotr Bacior - Portfolio Project 2026
+
+namespace ConsoleGames.Games
+{
+    // Tworzę klasę NumberGuessingGame, która będzie zawierać logikę gry w zgadywanie liczb.
+    public class NumberGuessingGame
+    {
+
+    }
+}
