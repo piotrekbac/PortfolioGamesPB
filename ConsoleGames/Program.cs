8d1d93fbe1b8d62da27db433870ae99227ee4f99
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-03 12:24:45 +0100

    Inicjalizacja projektu - PB

---
+++ ConsoleGames/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleGames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
