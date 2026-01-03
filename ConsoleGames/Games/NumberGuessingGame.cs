429c231e832d5947774a5d5d70a23cb83f97b7f9
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-03 12:46:36 +0100

    com2 - NumberGuessingGame - wywołanie metody DisplayHeader oraz komunikatów dla użytkownika jakie są zasady gry

--- ConsoleGames/Games/NumberGuessingGame.cs
+++ ConsoleGames/Games/NumberGuessingGame.cs
@@ -12,6 +12,15 @@ namespace ConsoleGames.Games
     // Tworzę klasę NumberGuessingGame, która będzie zawierać logikę gry w zgadywanie liczb.
     public class NumberGuessingGame
     {
+        public void Run()
+        {
+            // Wywołuję metodę DisplayHeader z klasy AuthorInfo, aby wyświetlić nagłówek gry
+            AuthorInfo.DisplayHeader("Gra w Zgadywanie Liczb");
 
+            // Wyświetlam powitanie użytkownika i instrukcje gry
+            Console.WriteLine("Cześć graczu! Wylosowałem liczbę z zakresu 1 do 100");
+            Console.WriteLine("Spróbuj ją zgadnąć w jak najmniejszej liczbie prób!\n");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        }
     }
 }
