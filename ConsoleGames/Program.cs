29202f6ad8d58cc5fdfdaf8a7b6fb21eb87c72cd
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-04 19:45:50 +0100

    com7 - program.cs - obsługwa wyboru użytkownika za pomocą switch

--- ConsoleGames/Program.cs
+++ ConsoleGames/Program.cs
@@ -29,6 +29,35 @@ namespace ConsoleGames
 
                 // Proszę użytkownika o wybór opcji
                 Console.Write("Twój wybór: ");
+
+                // Odczytuję wybór użytkownika z konsoli i przechowuję go w zmiennej "choise"
+                string choise = Console.ReadLine();
+
+                // Przetwarzam wybór użytkownika za pomocą instrukcji switch
+                switch (choise)
+                {
+                    // W przypadku wyboru "1" uruchamiam grę w zgadywanie liczb
+                    case "1": 
+                        NumberGuessingGame guessingGame = new NumberGuessingGame();
+                        guessingGame.Run();
+                        break;
+
+                    // W przypadku wyboru "0" ustawiam zmienną sterującą na false, aby zakończyć program
+                    case "0":
+                        keepRunning = false;
+                        break;
+
+                    // W przypadku nieprawidłowego wyboru informuję użytkownika i proszę o ponowny wybór
+                    default: 
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nNieprawidłowy wybór. Proszę spróbować ponownie.\n");
+                        Console.ResetColor();
+
+                        // Czekam na naciśnięcie klawisza przez użytkownika przed ponownym wyświetleniem menu
+                        Console.ReadKey();
+                        break;
+
+                }
             }
             
             
