3d4210bba905f55ae96b81db0837a6f9adb0872b
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-06 12:38:09 +0100

    com1 - TicTacToeGame.cs - inicjalizacja pliku

---
+++ ConsoleGames/Games/TicTacToeGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Piotr Bacior - Portfolio Project 2026
+
+namespace ConsoleGames.Games
+{
+    // Klasa reprezentująca grę w kółko i krzyżyk (Tic Tac Toe)
+    public class TicTacToeGame
+    {
+
+    }
+}
