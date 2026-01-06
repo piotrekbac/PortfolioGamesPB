ce704991ff97cb37d39dcaa7dee80143eb21fa2e
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-06 12:40:17 +0100

    com2 - TicTacToeGame.cs - definiowanie kluczowych zmiennych początkowych

--- ConsoleGames/Games/TicTacToeGame.cs
+++ ConsoleGames/Games/TicTacToeGame.cs
@@ -11,6 +11,11 @@ namespace ConsoleGames.Games
     // Klasa reprezentująca grę w kółko i krzyżyk (Tic Tac Toe)
     public class TicTacToeGame
     {
+        // Konstruktor klasy TicTacToeGame, inicjalizujący planszę i ustawiający aktualnego gracza na 'X'
+        // Używam planszy 3x3 do gry w kółko i krzyżyk, będzie to tablica dwuwymiarowa typu char 
 
+        private char[,] board;          // Plansza do gry
+        private char currentPlayer;     // Aktualny gracz ('X' lub 'O')
+        private int turnsCount;         // Liczba wykonanych ruchów
     }
 }
