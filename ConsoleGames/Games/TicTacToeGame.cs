c4db5f0cfcccdfa81ce7edb9fac5ecae3b103010
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-06 12:42:30 +0100

    com3 - TicTacToeGame.cs - inicjalizacja planszy, początkowych wartości i aktualnych stanów gry

--- ConsoleGames/Games/TicTacToeGame.cs
+++ ConsoleGames/Games/TicTacToeGame.cs
@@ -17,5 +17,21 @@ namespace ConsoleGames.Games
         private char[,] board;          // Plansza do gry
         private char currentPlayer;     // Aktualny gracz ('X' lub 'O')
         private int turnsCount;         // Liczba wykonanych ruchów
+
+        // Metoda uruchamiająca grę w kółko i krzyżyk
+        public void Run()
+        {
+            // Inicjalizuję planszę i ustawiam początkowe wartości
+            board = new char[3, 3];
+            {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+            };
+
+            currentPlayer = 'X';        // Aktualny gracz zaczyna jako 'X'
+            turnsCount = 0;             // Liczba ruchów na początku wynosi 0
+            bool gameEnded = false;     // Flaga wskazująca, czy gra się zakończyła
+        }
     }
 }
