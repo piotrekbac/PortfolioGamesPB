2739b43585daea6737c64fca5ca4bc7c9f67ec51
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-11 00:26:58 +0100

    com1 - HangmanGame.cs - inicjalizacja klasy HangmanGame

---
+++ ConsoleGames/Games/HangmanGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using ConsoleGames.Helpers;
+
+// Piotr Bacior - Portfolio Project 2026
+
+namespace ConsoleGames.Games
+{
+    // Tworzę klasę HangmanGame, która będzie zawierać logikę gry w wisielca.
+    public class HangmanGame
+    {
+
+    }
+}
