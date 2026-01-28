da84dee1bd2aa20ed2700fae0313e194aed6cb71
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-01-28 03:05:34 +0100

    com158 - SnakeGame.cs - dodaję deklarację użytkowania System.IO do obsługi plików - deklaracja na samym początku pliku

--- ConsoleGames/Games/SnakeGame.cs
+++ ConsoleGames/Games/SnakeGame.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;    // Dodane dla obsługi plików
 
 // Piotr Bacior - Portfolio Project 2026
 
