1e53e3af9d5c5302546eb85bc79092e82eb4f0c6
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-02 20:39:05 +0100

    com221 - SnakeGame.cs - wypisuję legendę gry - inicjalizacja

--- ConsoleGames/Games/SnakeGame.cs
+++ ConsoleGames/Games/SnakeGame.cs
@@ -319,6 +319,7 @@ namespace ConsoleGames.Games
             DrawCenteredText(" Sterowanie: Strzałki ", centerY + 4, ConsoleColor.Gray);                     // Rysuję instrukcję sterowania grą
 
             DrawCenteredText(" Cel: Zdobądź " + WinningScore + " punktów", centerY + 5, ConsoleColor.Gray);       // Rysuję cel gry z progiem punktowym do zwycięstwa
+            DrawCenteredText(" Legenda: ", centerY + 7, ConsoleColor.Gray);                                       // Rysuję legendę gry - inicjalizacja
         }
 
         // Definiuję metodę rysującą wyśrodkowany tekst w konsoli - metoda pomocnicza
