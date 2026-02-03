f96f4a6608f1e4ac13cea4fb02561e17c49da32e
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-03 04:12:15 +0100

    com235 - SnakeGame.cs - konfigurowanie konsoli dla gry - wywoływanie metody w metodzie głównej - Run

--- ConsoleGames/Games/SnakeGame.cs
+++ ConsoleGames/Games/SnakeGame.cs
@@ -65,6 +65,7 @@ namespace ConsoleGames.Games
             Console.CursorVisible = false;      // Ukrywam kursor konsoli
             Console.Clear();                    // Czyści ekran konsoli
 
+            SetupConsole();                     // Konfiguruję konsolę dla gry
             highScore = LoadHighScore();        // Ładuję najwyższy wynik z pliku
 
             // Inicjalizacja węża jako lista punktów na planszy, gdzie index 0 to głowa
