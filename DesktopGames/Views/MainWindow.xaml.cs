6d88bfa18942f7c4c1b5911ef57fbbbe9253379d
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-05 09:10:38 +0100

    com265 - SnakeGame.cs - przeniesienie pliku MainWindow.xaml do folderu Views wraz z odpowiednimi zmianami przestrzeni nazw

--- DesktopGames/Views/MainWindow.xaml.cs
+++ DesktopGames/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DesktopGames
+namespace DesktopGames.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
