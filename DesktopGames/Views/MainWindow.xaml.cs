4917966e78ffba15f3c2e07da36ef58b8e7a85d1
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-06 02:50:17 +0100

    com1 - MianWindow.xaml.cs - ustawienie wyjściowe pliku

--- DesktopGames/Views/MainWindow.xaml.cs
+++ DesktopGames/Views/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// Piotr Bacior - Projekt portfolio 2026 
+
 namespace DesktopGames.Views
 {
     /// <summary>
