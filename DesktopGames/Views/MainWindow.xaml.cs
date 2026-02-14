b3d98b19c67175d5218636320190a8ab56742bbf
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-15 00:23:09 +0100

    com 120 - MainWindow.xaml.cs - ukrycie nakładki z komunikatem o zakończeniu gry

--- DesktopGames/Views/MainWindow.xaml.cs
+++ DesktopGames/Views/MainWindow.xaml.cs
@@ -68,8 +68,9 @@ namespace DesktopGames.Views
 
             // Resetuję UI - ustawiam tekst zegara i licznik ruchów
 
-            TimeTextBlock.Text = "Czas: 0 s";     // Ustawiam tekst zegara na początkowy stan
-            MovesTextBlock.Text = "Ruchy: 0";     // Ustawiam tekst licznika ruchów na początkowy stan
+            TimeTextBlock.Text = "Czas: 0 s";                     // Ustawiam tekst zegara na początkowy stan
+            MovesTextBlock.Text = "Ruchy: 0";                     // Ustawiam tekst licznika ruchów na początkowy stan
+            GameOverOverlay.Visibility = Visibility.Collapsed;    // Ukrywam nakładkę z komunikatem o zakończeniu gry, jeśli była widoczna
 
             // Ustawiamy timer 
             if (timer != null)
