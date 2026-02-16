41efb678a728b120909b8e29c454afe136f8bfde
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-16 17:41:54 +0100

    com134 - MainWindow.xaml.cs - ustawianie tła drugiego klikniętego przycisku na kolor wcześniej już zdefiniowanego - klikniętgo przycisku (ErrorBrush)

--- DesktopGames/Views/MainWindow.xaml.cs
+++ DesktopGames/Views/MainWindow.xaml.cs
@@ -204,6 +204,7 @@ namespace DesktopGames.Views
                 isGameLocked = true;                     // Blokuję grę, aby użytkownik nie mógł klikać innych przycisków podczas animacji zakrywania kart
 
                 firstClicked.Background = ErrorBrush;    // Ustawiam tło pierwszego klikniętego przycisku na kolor dla błędnych par, aby oznaczyć, że ta para jest błędna
+                secondClicked.Background = ErrorBrush;   // Ustawiam tło drugiego klikniętego przycisku na kolor dla błędnych par, aby oznaczyć, że ta para jest błędna
 
                 // Jeżeli pary są różne to je zakrywamy z opóźnieniem, aby gracz miał czas zobaczyć, co było pod nimi
                 // Używam DispatcherTimer do opóźnienia zakrycia kart
