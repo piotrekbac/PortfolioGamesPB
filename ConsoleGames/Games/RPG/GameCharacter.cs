efc507fcf860301808ef91d3a5bb4661ba7e84d0
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-18 19:47:25 +0100

    com153 - MainWindow.xaml.cs - inicjalizacja klasy GameCharacter.cs

---
+++ ConsoleGames/Games/RPG/GameCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Piotr Bacior - 2026 - Console Games - RPG Game - GameCharacter class
+
+namespace ConsoleGames.Games.RPG
+{
+    // Klasa bazowa dla postaci w grze RPG
+    public abstract class GameCharacter
+    { 
+
+    }
+}
