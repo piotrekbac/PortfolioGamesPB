a9bd16f3af7a0c77883f36873603ea9be0492471
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-02-18 19:56:41 +0100

    com14 - GameCharacter - zdrowie postaci wewnątrz konstruktora GameCharacter

--- ConsoleGames/Games/RPG/GameCharacter.cs
+++ ConsoleGames/Games/RPG/GameCharacter.cs
@@ -29,6 +29,7 @@ namespace ConsoleGames.Games.RPG
         {
             Name = name;                   // Ustawiamy nazwę postaci
             MaxHealth = health;            // Ustawiamy maksymalny poziom życia postaci
+            Health = health;               // Ustawiamy aktualny poziom życia postaci
         }
     }
 }
